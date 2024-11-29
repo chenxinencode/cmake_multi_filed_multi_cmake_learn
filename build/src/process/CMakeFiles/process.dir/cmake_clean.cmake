c74c57b1538653da2ccf9906abbdc4747aba5f55
unknown <1113753662@qq.com>
2024-11-29 15:11:39 +0800

    version 0.0.1

---
+++ build/src/process/CMakeFiles/process.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/process.dir/process.cpp.obj"
+  "CMakeFiles/process.dir/process.cpp.obj.d"
+  "E:/c_learn/cmake_multi_filed_multi_cmake_learn/bin/libprocess.dll"
+  "E:/c_learn/cmake_multi_filed_multi_cmake_learn/bin/libprocess.dll.manifest"
+  "E:/c_learn/cmake_multi_filed_multi_cmake_learn/bin/libprocess.pdb"
+  "libprocess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/process.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
